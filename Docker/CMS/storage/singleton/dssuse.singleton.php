afe84f04b42f376cc651a97ca4b2f1c65205c3bd
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-04-08 11:55:25 +0100

    Defult files CMS

---
+++ Docker/CMS/storage/singleton/dssuse.singleton.php
@@ -0,0 +1,53 @@
+<?php
+ return array (
+  'name' => 'dssuse',
+  'label' => 'DSS use',
+  '_id' => 'dssuse5e45ccf6e1428',
+  'fields' => 
+  array (
+    0 => 
+    array (
+      'name' => 'description',
+      'label' => '',
+      'type' => 'text',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+    1 => 
+    array (
+      'name' => 'image',
+      'label' => '',
+      'type' => 'image',
+      'default' => '',
+      'info' => '',
+      'group' => '',
+      'localize' => false,
+      'options' => 
+      array (
+      ),
+      'width' => '1-1',
+      'lst' => true,
+      'acl' => 
+      array (
+      ),
+    ),
+  ),
+  'template' => '',
+  'data' => NULL,
+  '_created' => 1581632758,
+  '_modified' => 1582047490,
+  'description' => '',
+  'acl' => 
+  array (
+  ),
+);
